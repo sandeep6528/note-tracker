bfd1faac5e1b9a2d41cc21b2bc2d3dd80c923c33
Sandeep Meegahapola <smeegahapola5@gmail.com>
2023-12-01 19:27:38 -0600

    basic server configuration

---
+++ server.js
@@ -0,0 +1,22 @@
+const exp = require("constants")
+const express = require("express")
+const path = require("path")
+const PORT = process.env.PORT|| 3001
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}))
+app.use(express.static("public"))
+app.get("/notes",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/public/notes.html"))
+})
+app.get('*', (req, res) => {
+    console.log('im the get route')
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+
+app.listen(PORT,()=>{
+    console.log("app listening on PORT:" +PORT)
+})
